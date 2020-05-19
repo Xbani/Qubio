120e83aa75ce84e1b5579825f633edeba89864ee
Oscar Bonduelle <oscar.bonduelle@hotmail.fr>
2020-05-19 23:45:24 +0200

    Update quunplayablecharacter.h

--- objects/entities/quunplayablecharacter.h
+++ objects/entities/quunplayablecharacter.h
@@ -1,11 +1,27 @@
 #ifndef QUUNPLAYABLECHARACTER_H
 #define QUUNPLAYABLECHARACTER_H
 
+#include "qucharacter.h"
 
+
+/**
+ * @brief The QuUnplayableCharacter class
+ * @details Class which is a player controlled by another client.
+ */
 class QuUnplayableCharacter : public QuCharacter
 {
 public:
     QuUnplayableCharacter();
+    QuUnplayableCharacter(int instance_id);
+
+    // QuObject interface
+public:
+    QJsonObject toJSON();
+    void fromJSON(QJsonObject qJsonObject);
+
+    // QuEntity interface
+public:
+    int getClassId();
 };
 
 #endif // QUUNPLAYABLECHARACTER_H
