267d4a99ec3eaaa07b0766a05f9dd1762558c871
Oscar Bonduelle <oscar.bonduelle@hotmail.fr>
2020-05-19 20:09:25 +0200

    Update QuBlock Class
    - Add QImage sprite private attribute + public getter : QImage getSprite()
    - Add int texture_id private attribute + public getter : int getTextureId()
    - Add int MAX_TEXTURE_ID public static attribute
    
    - Add QuBlock(int texture_block_id) constructor
    - Modify QuBlock() construtor

--- objects/qublock.cpp
+++ objects/qublock.cpp
@@ -1,6 +1,25 @@
 #include "qublock.h"
 
-QuBlock::QuBlock()
+QuBlock::QuBlock():QuBlock(0)
 {
 
+}
+
+QuBlock::QuBlock(int texture_id)
+{
+    texture_id=texture_id;
+    QString path=QString(":/resources/block");
+    path.append(QString::number(texture_id));
+    path.append(".png");
+    sprite=QImage(path);
+}
+
+QImage QuBlock::getSprite()
+{
+    return sprite;
+}
+
+int QuBlock::getTextureId()
+{
+    return texture_id;
 }
--- objects/qublock.h
+++ objects/qublock.h
@@ -10,8 +10,18 @@
  */
 class QuBlock : public QuObject
 {
+private:
+    QImage sprite;
+    int texture_id;
 public:
     QuBlock();
+    QuBlock(int texture_id);
+
+    QImage getSprite();
+    int getTextureId();
+
+public:
+    static const int MAX_TEXTURE_ID=1;
 };
 
 #endif // QUBLOCK_H
