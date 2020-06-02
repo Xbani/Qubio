957ff31cc0093da649808f833efe1ee599c22d33
theoperdrielPCFixe <tperdriel gmail.com>
2020-06-02 20:42:18 +0200

    enlever les include du main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,6 @@
 #include "mainwindow.h"
 #include "qugameengine.h"
-
-#include "objects\quobject.h"
-#include "objects\quentity.h"
-#include "objects\qublock.h"
-#include "objects\\blocks\qusolidblock.h"
-#include "objects\\entities\quplayablecharacter.h"
-#include "objects\\entities\quunplayablecharacter.h"
-#include "charactercontroller.h"
-
 #include <QApplication>
-#include <QtWidgets>
-#include <QGraphicsView>
-
-#include <objects/quobject.h>
-
-#include <rooms/ui/quuimainmenu.h>
-
-#include <rooms/qubuildermapframe.h>
-#include <rooms/qumapbuilder.h>
 
 int main(int argc, char *argv[])
 {
