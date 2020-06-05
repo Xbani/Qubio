ecac9361c9ff4357c88f3015c545f4227b895c7d
florianB42 <florianbartholin@orange.fr>
2020-06-05 13:46:03 +0200

    Update quphysicsconst.h

--- quphysicsconst.h
+++ quphysicsconst.h
@@ -8,9 +8,9 @@ public:
     QuPhysicsConst();
     constexpr static double G_FORCE  = 45;
     constexpr static double MAX_VITESSE_HORIZONTALE = 9;
-    constexpr static double MAX_VITESSE_VERTICALE = 15;
+    constexpr static double MAX_VITESSE_VERTICALE = 20;
     constexpr static double ACC_LATERAL_MOUVEMENT  = 45;
-    constexpr static double ACC_JUMP  = 1600;
+    constexpr static double ACC_JUMP  = 800;
     constexpr static double APPROX_COS_PI_4 = 0.707;
     constexpr static double INERTIA = 2;
     constexpr static double CHARACTER_WIDTH = 80;
